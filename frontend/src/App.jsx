5566eebedc1545ae3f481864fc84b087e5080ff7
Alysha Huot <alyshajhuot@gmail.com>
2024-02-28 14:51:26 -0700

    use mock data for photos and topic lists

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 
-import PhotoList from 'components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 
-// const createPhotoList = (numOfPhotos) => {
-  // const photoArray = [...Array(numOfPhotos)].map((_, i) => ({
-  //   ...sampleDataForPhotoListItem,
-  //   id: `${i}`
-  // }));
-
-//   return photoArray.map((photo, index) => (
-//     <PhotoListItem key={photo.id} photo={photo} />
-//   ))
-// }
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,24 +3,9 @@ import React from "react";
 import "../styles/TopicList.scss";
 import "../styles/TopicListItem.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const sampleDataForTopicList = topics;
 
 const TopicList = () => {
   return (
