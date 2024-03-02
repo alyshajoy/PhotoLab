4bd004fc4ae423577d3ac0ad5a5bb47f9a79d431
Alysha Huot <alyshajhuot@gmail.com>
2024-03-01 18:25:04 -0700

    refactor usApplicationData to use useReducer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,32 +1,46 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import topics from 'mocks/topics';
 import photos from 'mocks/photos';
 
+const initialState = {
+  fav: [],
+  isModalOpen: false,
+  focusedPhoto: undefined,
+  topics,
+  photos
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM_TO_FAV':
+      return { ...state, fav: [...state.fav, action.payload] };
+    case 'TOGGLE_MODAL':
+      return { ...state, isModalOpen: !state.isModalOpen };
+    case 'SET_FOCUSED_PHOTO':
+      return { ...state, focusedPhoto: action.payload };
+    default:
+      throw new Error();
+  }
+}
+
 export default function useApplicationData() {
-  const [fav, setFav] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [focusedPhoto, setFocusedPhoto] = useState();
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const addItemToFav = (item) => {
-    setFav([...fav, item]);
+    dispatch({ type: 'ADD_ITEM_TO_FAV', payload: item });
   };
 
   const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
+    dispatch({ type: 'TOGGLE_MODAL' });
   };
 
   const getFocusedPhotoData = (photo) => {
-    setFocusedPhoto(photo);
+    dispatch({ type: 'SET_FOCUSED_PHOTO', payload: photo });
   }
 
   return {
-    state: {
-      fav,
-      isModalOpen,
-      focusedPhoto,
-      topics,
-      photos
-    },
+    state,
     addItemToFav,
     toggleModal,
     getFocusedPhotoData
