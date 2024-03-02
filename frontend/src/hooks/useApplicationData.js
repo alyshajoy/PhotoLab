7b44e19aea448fc2df182480c53ad76a99cbac42
Alysha Huot <alyshajhuot@gmail.com>
2024-03-01 23:40:27 -0700

    add useEffect to retrieve photo and topic data from APIs

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,14 +1,16 @@
-import { useReducer } from 'react';
-import topics from 'mocks/topics';
-import photos from 'mocks/photos';
+import { useReducer, useEffect } from 'react';
 
 const initialState = {
   fav: [],
   isModalOpen: false,
   focusedPhoto: undefined,
-  topics,
-  photos
+  topics: [],
+  photos: []
 };
+
+const GET_PHOTOS_URL = "http://localhost:8001/api/photos";
+const GET_TOPICS_URL = "http://localhost:8001/api/topics";
+const GET_PHOTOS_BY_TOPICS_URL = "http://localhost:8001/api/topics/photos/:topic_id";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -18,14 +20,29 @@ const reducer = (state, action) => {
       return { ...state, isModalOpen: !state.isModalOpen };
     case 'SET_FOCUSED_PHOTO':
       return { ...state, focusedPhoto: action.payload };
+    case 'SET_PHOTO_DATA':
+      return {...state, photos: action.payload};
+    case 'SET_TOPIC_DATA':
+      return {...state, topics: action.payload};
     default:
       throw new Error("The type stated doesn't exist.");
   }
 }
 
 export default function useApplicationData() {
-
   const [state, dispatch] = useReducer(reducer, initialState);
+  
+  useEffect(() => {
+    fetch(GET_PHOTOS_URL)
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'SET_PHOTO_DATA', payload: data}))
+  }, [])
+
+  useEffect(() => {
+    fetch(GET_TOPICS_URL)
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'SET_TOPIC_DATA', payload: data}))
+  }, [])
 
   const addItemToFav = (item) => {
     dispatch({ type: 'ADD_ITEM_TO_FAV', payload: item });
